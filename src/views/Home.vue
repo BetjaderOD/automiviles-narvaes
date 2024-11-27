d3940d405671f7b278228745042830b62df0bb41
Ubuntu <ubuntu@ip-172-31-30-58.ec2.internal>
2024-11-27 02:14:39 +0000

    Hola

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <!-- src/views/Home.vue -->
 <template>
     <div>
-      <h1>Homssssspøsssse</h1>
+      <h1>Howwwwmssssspøsssse</h1>
       <p>Bienvenido a la página de inicio.</p>
     </div>
   </template>
